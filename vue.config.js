1358a34dd841408f35ff6f496f5ab93de8313f6e
vision1234 <709318017@qq.com>
2023-11-29 17:31:46 +0800

    m

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 8083, // 你希望使用的端口号
+  },
 });
